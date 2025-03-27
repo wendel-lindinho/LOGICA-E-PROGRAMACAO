245adbb8dffd39f857ef533319e2a1406e861827
Wendel Ferreira Martins <iiwendelmartins8@gmail.com>
2025-03-27 10:49:33 -0300

    Add files via upload

---
+++ aula 13/condicionais/notas.js
@@ -0,0 +1,20 @@
+var N1 = 1
+var N2 = 2
+
+
+var media  = (N1 + N2)/2
+
+if (media > 5){
+    console.log(media, "aprovado");
+    
+}
+
+else if  (media >= 3){
+    console.log(media, "recuperação");
+    
+}
+
+else {
+    console.log(media, "reprovado");
+    
+}
