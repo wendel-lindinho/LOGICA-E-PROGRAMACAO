6f5d33b11ba7de797939a1a880bf8071b2bc181a
Wendel Ferreira Martins <iiwendelmartins8@gmail.com>
2025-05-15 10:57:51 -0300

    Add files via upload

---
+++ crudphp/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina Home</title>
+    <link rel="stylesheet" href="estilos/style.css">
+</head>
+<body>
+    <h1>Bem-Vindo ao Sistemas de casastro</h1>
+    <p>Escolha uma das pessoas a baixo</p>
+
+    <div class="menu">
+    <a href="back/cadastro.php">cadastro</a>
+    <a href="back/verificarCadastro.php">Verificar cadastro</a>
+    <a href="">Atualizar Cadastro</a>
+    <a href="">Verificar Nota</a>
+    <a href="">Atualizar Nota</a>
+    </div>
+    
+
+
+</body>
+</html>
